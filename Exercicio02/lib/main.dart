1e6ad09b8d743a00058e1d2991bbb7a28048d15e
Mariana Cavalcanti Machado Anselmo <104873264+Marih935@users.noreply.github.com>
2024-03-27 23:50:55 -0300

    Add files via upload

---
+++ Exercicio02/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PrimeiraTela(),
+    );
+  }
+}
+
+class PrimeiraTela extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Primeira Tela'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SegundaTela()),
+            );
+          },
+          child: Text('Ir para Segunda Tela'),
+        ),
+      ),
+    );
+  }
+}
+
+class SegundaTela extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Segunda Tela'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => PrimeiraTela()),
+            );
+          },
+          child: Text('Voltar para Primeira Tela'),
+        ),
+      ),
+    );
+  }
+}
